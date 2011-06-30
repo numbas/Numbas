0b0518bb0701c4afd2dc9b36cfc58594c30ae14c
Christian Perfect <christianperfect@gmail.com>
2011-06-30 14:14:22 +0100

    changed the steps marking algorithm.
    A problem arose with steps marking today, and there was no
    solution with the current steps marking algorithm.
    Stacey set up a problem as follows:
    - The part is worth 2 marks if you get it right.
    - Steps are available, but looking at them will cost you a mark.
    - There is a single marked step, worth 0.5 marks.
    
    So what should happen is as follows:
    - Getting the part right without looking at steps gets you 2 marks
    - Getting the part right after looking at steps gets you at most 1 mark
    - Getting the final solution wrong but the step right gets you half a mark.
    - You should get more points for getting the final solution but not the
      steps, than for getting the steps but not the final solution.
    
    With the current algorithm, this is four constraints on three variables,
    and there's no solution.
    
    Additionally, I think that if, after revealing steps, you get the final
    solution right, it shouldn't matter what you wrote in the steps box.
    This way, students who just needed a little hint don't have to plod
    through putting intermediate results in.
    
    So this is the formula I came up with:
    
    Score awarded = (partMarks - stepsPenalty) * partCredit +
    				(1-partCredit)*stepsMarks*stepsCredit
    
    partCredit and stepsCredit are numbers in the range [0,1] representing
    how correct the student's answers to the final solution and the steps,
    respectively, were.
    
    With this formula, setting the following values for the part gives
    the desired behaviour:
    partMarks = 2
    stepsPenalty = 1
    stepsMarks = 0.5
    
    The fourth constraint is satisfied because partMarks is greater than
    stepsMarks.

--- runtime/scripts/question.js
+++ runtime/scripts/question.js
@@ -522,7 +522,6 @@ function Part( xml, path, question, parentPart, loading )
 		this.steps[i] = step;
 		this.stepsMarks += step.marks;
 	}
-	this.marks += this.stepsMarks;
 
 	//initialise display code
 	this.display = new Numbas.display.PartDisplay(this);
@@ -570,18 +569,21 @@ Part.prototype = {
 	{
 		if(this.steps.length && this.stepsShown)
 		{
-			this.score = (this.marks - this.stepsMarks) * this.credit; 	//score for main keypart
+			this.score = (this.marks - this.settings.stepsPenalty) * this.credit; 	//score for main keypart
 
+			var stepsScore = 0;
 			for(var i=0; i<this.steps.length; i++)
 			{
-				this.score += this.steps[i].score;
+				stepsScore += this.steps[i].score;
 			}
-			this.score -= this.settings.stepsPenalty;
 
-			if(this.settings.enableMinimumMarks && this.score < this.settings.minimumMarks)
-			{
-				this.score = this.settings.minimumMarks;
-			}
+			var stepsFraction = Math.max(Math.min(1-this.credit,1),0);	//any credit not earned in main part can be earned back in steps
+			stepsScore *= stepsFraction;
+			this.score += stepsScore;									//add score from steps to total score
+			this.score = Math.min(this.score,this.marks - this.settings.stepsPenalty)	//if too many marks are awarded for steps, it's possible that getting all the steps right leads to a higher score than just getting the part right. Clip the score to avoid this.
+
+			if(this.settings.enableMinimumMarks)								//make sure awarded score is not less than minimum allowed
+				this.score = Math.max(this.score,this.settings.minimumMarks);
 		}
 		else
 		{
@@ -619,6 +621,11 @@ Part.prototype = {
 			this.reportStudentAnswer(this.studentAnswer);
 		else
 			this.reportStudentAnswer('');
+
+		for(var i=0;i<this.steps.length;i++)
+		{
+			this.steps[i].submit();
+		}
 
 
 		this.calculateScore();
