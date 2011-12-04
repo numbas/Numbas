97abc1184a29473285df195caf3bba093d394622
Christian Perfect <christianperfect@gmail.com>
2011-12-04 11:48:22 +0000

    add shiv for Array.reduce
    because IE doesn't implement Array.reduce.

--- runtime/scripts/util.js
+++ runtime/scripts/util.js
@@ -414,4 +414,31 @@ if (!Array.prototype.map)
   };
 }
 
+//from https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/Reduce 
+if ( !Array.prototype.reduce ) {
+  Array.prototype.reduce = function reduce(accumulator){
+        var i, l = this.length, curr;
+        
+        if(typeof accumulator !== "function") // ES5 : "If IsCallable(callbackfn) is false, throw a TypeError exception."
+          throw new TypeError("First argument is not callable");
+
+        if((l == 0 || l === null) && (arguments.length <= 1))// == on purpose to test 0 and false.
+          throw new TypeError("Array length is 0 and no second argument");
+        
+        if(arguments.length <= 1){
+          curr = this[0]; // Increase i to start searching the secondly defined element in the array
+          i = 1; // start accumulating at the second element
+        }
+        else{
+          curr = arguments[1];
+        }
+        
+        for(i = i || 0 ; i < l ; ++i){
+          if(i in this)
+            curr = accumulator.call(undefined, curr, this[i], i, this);
+        }
+        
+        return curr;
+      };
+  }
 });
