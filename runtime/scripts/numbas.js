6b6f8d44b7717ff31f3ac6766a2550156c6f3349
Christian Perfect <christianperfect@gmail.com>
2012-04-18 13:21:06 +0100

    move Numbas.init into exam.js
    This should make it easier for other projects to just load numbas.js and the files they need, then defining their own Numbas.init function.

--- runtime/scripts/numbas.js
+++ runtime/scripts/numbas.js
@@ -68,66 +68,6 @@ Numbas.Error = function(message)
 Numbas.Error.prototype = Error.prototype;
 Numbas.Error.prototype.constructor = Numbas.Error;
 
-// Initialise the exam:
-// - Connect to the LMS, which might have saved student answers
-// - Load the exam XML and the XSL templates
-// - create and initialise the exam object
-// - display the frontpage
-// This function is called when all the other scripts have been loaded and executed. 
-// It uses the scheduling system to make sure the browser isn't locked up when the exam is being initialised
-var init = Numbas.init = function()
-{
-	var job = Numbas.schedule.add;
-
-	//job(function(){Numbas.timing.start()});			//start timing (for performance tuning)
-
-	job(Numbas.xml.loadXMLDocs);				//load in all the XML and XSLT files
-
-	job(function()
-	{
-		var store = Numbas.store = new Numbas.storage.SCORMStorage();	//The storage object manages communication between the LMS and the exam
-		
-		var exam = Numbas.exam = new Numbas.Exam();					//create the exam object, and load in everything from the XML
-
-		switch(store.getEntry())
-		{
-		case 'ab-initio':
-			job(exam.init,exam);
-			job(Numbas.display.init);
-			job(function() {
-				if(exam.showFrontPage)
-				{
-					exam.display.showInfoPage('frontpage');
-				}
-				else
-				{
-					exam.begin();
-				}
-			});	
-			break;
-
-		case 'resume':
-			job(exam.load,exam);
-			job(Numbas.display.init);
-
-			job(function() {
-				if(exam.currentQuestion !== undefined)
-				{
-					job(exam.display.showInfoPage,exam.display,'suspend');
-				}
-				else
-				{
-					job(exam.display.showInfoPage,exam.display,'frontpage');
-				}
-			});
-
-			break;
-		}
-		//job(function(){Numbas.timing.end('init');});			//end performance timing 
-	});
-
-}
-
 // Script loading system.
 // call loadScript to load a file. It will then call queueScript with a list of its dependencies, and its code wrapped into a callback function.
 // When all the dependencies have been loaded, the callback function is executed. The callback function should finish with a call to scriptLoaded, which keeps track of what's been run.
