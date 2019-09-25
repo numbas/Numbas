numbas/Numbas

commit bc131f838030766df63e9c6fe7ec4cb4b6729f4b

resolves #605

runtime/scripts/math.js
@@ -1656,6 +1656,18 @@ var math = Numbas.math = /** @lends Numbas.math */ {
             total = math.add(total,list[i]);
         }
         return total;
+    },
+    /** Multiplies the elements in the given list
+     *
+     * @param {Array.<Number>} list
+     * @returns {Number}
+     */
+    prod: function(list)  {
+        let product = 1;
+        for (let i = 0; i < list.length; i++){
+            product = math.mul(product, list[i]);
+        }
+        return product;
     }
 };
 math.gcf = math.gcd;

runtime/scripts/jme-builtins.js
@@ -643,6 +643,8 @@ newBuiltin('fract',[TDecimal], TDecimal, function(a) {return a.re.minus(a.re.tru
 
 newBuiltin('sum',[sig.listof(sig.type('number'))],TNum,math.sum,{unwrapValues: true});
 newBuiltin('sum',[TVector],TNum,math.sum);
+newBuiltin('prod',[sig.listof(sig.type('number'))],TNum,math.prod,{unwrapValues: true});
+newBuiltin('prod',[TVector],TNum,math.prod);
 newBuiltin('deal',[TNum],TList,
     function(n) {
         return math.deal(n).map(function(i) {

commit 5488753c2778faa303b4f07b60368a92e45c6df9

change 'let' to 'var'
introduced accidentally in #609.

We're still on ES5, so can't use 'let' syntax.

runtime/scripts/math.js
@@ -1738,8 +1738,8 @@ var math = Numbas.math = /** @lends Numbas.math */ {
      * @returns {Number}
      */
     prod: function(list)  {
-        let product = 1;
-        for (let i = 0; i < list.length; i++){
+        var product = 1;
+        for (var i = 0; i < list.length; i++){
             product = math.mul(product, list[i]);
         }
         return product;