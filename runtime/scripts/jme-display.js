a98fe62981e27f82dd3fbcd86a0d25cba48f528a
Thomas Pilewicz <thomas.pwcz@gmail.com>
2019-10-12 12:27:38 +0200

    create flatFractions display flag
    The flag enables displaying fractions horizontally. It is checked in jme.display.texOps. It converts fractions into this latex: "\left. numerator \middle/ denominator \right."
    If a whitespace is present in the numerator or denominator, this means we need to enclose it in brackets for displaying clearly.

--- runtime/scripts/jme-display.js
+++ runtime/scripts/jme-display.js
@@ -311,7 +311,18 @@ var texOps = jme.display.texOps = {
         }
         return s;
     }),
-    '/': (function(thing,texArgs) { return ('\\frac{ '+texArgs[0]+' }{ '+texArgs[1]+' }'); }),
+    '/': (function(thing,texArgs,settings) {
+        if (settings.flatfractions) {
+            for (var i=0; i<2; i++) {
+                // identifying if the numerator and denominator need brackets around them by
+                // checking whether they contain whitespaces
+                if (texArgs[i].includes(' ')) texArgs[i] = '(' + texArgs[i] + ')'
+            }
+            return ('\\left. '+texArgs[0]+' \\middle/ '+texArgs[1]+' \\right.');
+        } else {
+            return ('\\frac{ '+texArgs[0]+' }{ '+texArgs[1]+' }');
+        }
+    }),
     '+': (function(thing,texArgs,settings) {
         var a = thing.args[0];
         var b = thing.args[1];
