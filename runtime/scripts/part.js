9003a586f73d40879e91388bc4b5e558889537f2
Christian Lawson-Perfect <christianperfect@gmail.com>
2020-01-22 15:33:33 +0000

    fix weird surplus feedback messages in gapfills
    After the change to show feedback items with empty messages, it became
    clear that the handling of gapfills led to an extra item for each gap.
    
    This commit changes Part.apply_feedback to add on to the current creditFraction directly, rather than calling Part.addCredit, which produces a markingFeedback item

--- runtime/scripts/part.js
+++ runtime/scripts/part.js
@@ -1050,8 +1050,7 @@ Part.prototype = /** @lends Numbas.parts.Part.prototype */ {
                 case 'end_lift':
                     var last_lift = lifts.pop();
                     var lift_credit = this.credit;
-                    this.creditFraction = last_lift.creditFraction;
-                    this.addCredit(lift_credit);
+                    this.creditFraction = last_lift.creditFraction.add(math.Fraction.fromFloat(lift_credit));
                     scale = last_lift.scale;
                     break;
             }
@@ -1062,6 +1061,8 @@ Part.prototype = /** @lends Numbas.parts.Part.prototype */ {
         }
         part.answered = valid;
 
+        /** Add marks awarded/taken away messages to the end of each feedback item which changes awarded credit
+         */
         var t = 0;
         for(var i=0;i<part.markingFeedback.length;i++) {
             var action = part.markingFeedback[i];
@@ -1075,6 +1076,9 @@ Part.prototype = /** @lends Numbas.parts.Part.prototype */ {
             t += change;
             t = Math.max(0,t);
             change = t-ot;
+            if(action.message===undefined) {
+                action.message = '';
+            }
             if(change!=0) {
                 if(util.isNonemptyHTML(action.message)) {
                     action.message += '\n\n';
