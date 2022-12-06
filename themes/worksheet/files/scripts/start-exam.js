cf1d8a34b1573dbe602983b1ea3b60461f6dbbfb
Christian Lawson-Perfect <christianperfect@gmail.com>
2022-12-06 09:56:08 +0000

    remove es6-promise dependency from the worksheet theme

--- themes/worksheet/files/scripts/start-exam.js
+++ themes/worksheet/files/scripts/start-exam.js
@@ -1,5 +1,5 @@
 // 'base' gives the third-party libraries on which Numbas depends
-Numbas.queueScript('base',['jquery','localisation','seedrandom','knockout','sarissa','es6-promise'],function() {
+Numbas.queueScript('base',['jquery','localisation','seedrandom','knockout','sarissa'],function() {
 });
 
 
