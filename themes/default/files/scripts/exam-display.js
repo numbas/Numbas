be09d194d0e95015b37e99887618b76cce4991a2
Lycanic <17724612+Lycanic@users.noreply.github.com>
2023-05-16 16:10:14 +0100

    Basic object-based implementation

--- themes/default/files/scripts/exam-display.js
+++ themes/default/files/scripts/exam-display.js
@@ -683,6 +683,8 @@ Numbas.queueScript('exam-display',['display-base','math','util','timing'],functi
             //endBOTCH
             let exam_object = Numbas.store.examSuspendData();
             exam_object.student_name = this.exam.student_name; //add any extra data here
+            exam_object.total_score = this.exam.score;
+            exam_object.possible_score = this.exam.mark;
             let contents = JSON.stringify(exam_object); //this will need to be a json of the exam object, which seems like it should be created somewhere already as we have ways to access it?
             let encryptedContents = await Numbas.download.encrypt(contents, this.exam.settings.csvEncryptionKey);
             encryptedContents = Numbas.download.b64encode(encryptedContents);
