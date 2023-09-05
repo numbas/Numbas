250795da8029bcd29d2098e377f6eea2176eca8e
Christian Lawson-Perfect <christianperfect@gmail.com>
2023-09-05 09:49:30 +0100

    add a shim for Date.toISOString

--- runtime/scripts/util.js
+++ runtime/scripts/util.js
@@ -1736,4 +1736,28 @@ if (!Object.entries) {
 }
 })();
 
+if (!Date.prototype.toISOString) {
+  (function() {
+
+    function pad(number) {
+      if (number < 10) {
+        return '0' + number;
+      }
+      return number;
+    }
+
+    Date.prototype.toISOString = function() {
+      return this.getUTCFullYear() +
+        '-' + pad(this.getUTCMonth() + 1) +
+        '-' + pad(this.getUTCDate()) +
+        'T' + pad(this.getUTCHours()) +
+        ':' + pad(this.getUTCMinutes()) +
+        ':' + pad(this.getUTCSeconds()) +
+        '.' + (this.getUTCMilliseconds() / 1000).toFixed(3).slice(2, 5) +
+        'Z';
+    };
+
+  }());
+}
+
 });
