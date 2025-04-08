d901c231649b3aaa85c2e77fbefa1d2e1bebf4e2
Christian Lawson-Perfect <christian.perfect@ncl.ac.uk>
2025-04-08 09:19:29 +0100

    remove sarissa dependency in the worksheet theme

--- themes/worksheet/files/scripts/start-exam.js
+++ themes/worksheet/files/scripts/start-exam.js
@@ -1,5 +1,5 @@
 // 'base' gives the third-party libraries on which Numbas depends
-Numbas.queueScript('base',['jquery','localisation','seedrandom','knockout','sarissa'],function() {
+Numbas.queueScript('base',['jquery','localisation','seedrandom','knockout'],function() {
 });
 
 
